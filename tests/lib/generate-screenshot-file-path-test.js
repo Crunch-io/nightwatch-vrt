be0a708809a161d5f69b2d373020acee58bd13df
Ian Brown <Ian.Brown@iress.co.uk>
2018-12-03 12:52:09 +0000

    backslash requires escape character

--- tests/lib/generate-screenshot-file-path-test.js
+++ tests/lib/generate-screenshot-file-path-test.js
@@ -24,12 +24,12 @@ describe('generateScreenshotFilePath', () => {
 
     it('should generate a file path by using the basePath parameter, and the current test name and module', () => {
         expect(generateScreenshotFilePath(getNightwatchClient(), 'baseline'))
-            .toEqual(`${process.cwd()}/baseline/visualizations/bar-plots.png`)
+            .toEqual(`${process.cwd()}\\baseline\\visualizations\\bar-plots.png`)
     })
 
     it('should generate a file path by using the basePath parameter, and a custom filename', () => {
         expect(generateScreenshotFilePath(getNightwatchClient(), 'baseline', 'foo-test'))
-            .toEqual(`${process.cwd()}/baseline/visualizations/foo-test.png`)
+            .toEqual(`${process.cwd()}\\baseline\\visualizations\\foo-test.png`)
     })
 
     it('should pass the correct filename to the custom naming function', () => {
@@ -37,6 +37,6 @@ describe('generateScreenshotFilePath', () => {
             path.join(process.cwd(), basePath, client.currentTest.module, `custom-${fileName}`)
 
         expect(generateScreenshotFilePath(getNightwatchClient(), 'baseline', 'foo-test'))
-            .toEqual(`${process.cwd()}/baseline/visualizations/custom-foo-test.png`)
+            .toEqual(`${process.cwd()}\\baseline\\visualizations\\custom-foo-test.png`)
     })
 })
