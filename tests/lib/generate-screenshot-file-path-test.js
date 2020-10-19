552a1860ff8ccd037c76a7d890636965200b4b7e
humphreyn <neville.humphrey@reedbusiness.com>
2020-10-19 16:57:25 +0100

    Resolved url-regex DOS vulnerability (https://www.npmjs.com/advisories/1550) which is a dependency of jimp v0.2.28 by upgrading jimp to 0.16.1
    updated test to be platform agnostic (work on windows)
    
    updated the following npm dependency packages to the latest versions:
    - jimp: 0.16.1
    - lodash: 4.17.20
    - eslint: 7.11.0
    - eslint-config-crunch: 1.8.0
    - eslint-plugin-react: 7.21.4
    - http-server: 0.12.3
    - jest: 26.6.0
    - nightwatch: 1.4.3
    - npm-run-all: 4.1.5
    - phantomjs-prebuilt: 2.1.16
    - rimraf: 3.0.2
    - selenium-standalone: 6.20.1

--- tests/lib/generate-screenshot-file-path-test.js
+++ tests/lib/generate-screenshot-file-path-test.js
@@ -18,12 +18,12 @@ describe('generateScreenshotFilePath', () => {
 
     it('should generate a file path by using the basePath parameter, and the current test name and module', () => {
         expect(generateScreenshotFilePath(getNightwatchClient(), 'baseline'))
-            .toEqual(`${process.cwd()}/baseline/visualizations/bar-plots.png`)
+            .toEqual(path.resolve(`${process.cwd()}/baseline/visualizations/bar-plots.png`))
     })
 
     it('should generate a file path by using the basePath parameter, and a custom filename', () => {
         expect(generateScreenshotFilePath(getNightwatchClient(), 'baseline', 'foo-test'))
-            .toEqual(`${process.cwd()}/baseline/visualizations/foo-test.png`)
+            .toEqual(path.resolve(`${process.cwd()}/baseline/visualizations/foo-test.png`))
     })
 
     it('should pass the correct filename to the custom naming function', () => {
@@ -34,10 +34,10 @@ describe('generateScreenshotFilePath', () => {
         set(nightwatchClient, 'globals.test_settings.visual_regression_settings.generate_screenshot_path', testFn)
 
         const result = generateScreenshotFilePath(nightwatchClient, 'baseline', 'foo-test')
+        const expected = path.resolve(`${process.cwd()}/baseline/visualizations/test-setting-custom-foo-test.png`)
 
         expect(testFn).toHaveBeenCalled()
-        expect(result)
-            .toEqual(`${process.cwd()}/baseline/visualizations/test-setting-custom-foo-test.png`)
+        expect(result).toEqual(expected)
     })
 
     it('should use the base global setting for generate_screenshot_path if a test_setting exists', () => {
@@ -46,7 +46,7 @@ describe('generateScreenshotFilePath', () => {
             path.join(process.cwd(), basePath, client.currentTest.module, `test-setting-custom-${fileName}`))
 
         set(nightwatchClient, 'globals.test_settings.visual_regression_settings.generate_screenshot_path', testFn)
-        
+
         const customFn = jest.fn((client, basePath, fileName) =>
             path.join(process.cwd(), basePath, client.currentTest.module, `custom-${fileName}`))
 
@@ -54,8 +54,8 @@ describe('generateScreenshotFilePath', () => {
         set(nightwatchClient, 'globals.test_settings.visual_regression_settings.generate_screenshot_path', testFn)
 
         const result = generateScreenshotFilePath(nightwatchClient, 'baseline', 'foo-test')
+        const expected = path.resolve(`${process.cwd()}/baseline/visualizations/custom-foo-test.png`)
 
-        expect(result, 'baseline', 'foo-test')
-            .toEqual(`${process.cwd()}/baseline/visualizations/custom-foo-test.png`)
+        expect(result).toEqual(expected)
     })
 })
